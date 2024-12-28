2c518f89efef90dc3ef7cd41c3d53be2e43a0d37
Abhinandan911-ux <abhinandanhavannavar383@gmail.com>
2024-12-28 12:17:02 +0530

    Delete authRouter.js

--- authRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const {signup,signin} = require('../controllers/authcontroller');
-const router = express.Router();
-
-router.post('/sigup',signup);
-router.post('/signin',signin);
-
-module.exports = router;
