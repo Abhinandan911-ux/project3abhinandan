d5dda499523dcd65c258ce8512953985e95aefdc
Abhinandan911-ux <abhinandanhavannavar383@gmail.com>
2024-12-28 12:16:25 +0530

    Delete User.js

--- User.js
+++
@@ -1,7 +0,0 @@
-const mongooes = require('monogoose');
-
-const userSchema = new mongooes.schema({
-    email:{type: String, required: true, unique: true },
-    password: {type: String, required: ture }
-});
-module.exports = mongooes.model('User',userSchema);
